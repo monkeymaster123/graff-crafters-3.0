79e350d50528b6695956566ff4e31ef3b8b96512
Dalton Craven <20dalton00@gmail.com>
2019-03-17 19:30:51 -0400

    design updates

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,34 +1,35 @@
 import React from 'react'
 
 const Primary = ({ data }) => (
-  <section
-    className="section section--gradient"
-    style={{ backgroundColor: 'lilac' }}
-  >
+  <div>
     <div className="container">
       <div className="section">
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="content">
-              <div className="content">
-                <div className="tile">
-                  <h1 className="title">{data.heading}</h1>
-                </div>
-                <div className="tile">
-                  <h3 className="subtitle">{data.subheading}</h3>
-                </div>
-                <div className="tile">
-                  <a href={data.link} class="button is-primary">
-                    Learn More
-                  </a>
-                </div>
+              <div className="tile">
+                <h1 className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen">
+                  {data.heading}
+                </h1>
+              </div>
+              <div className="tile">
+                <h3>{data.description}</h3>
+              </div>
+              <div className="tile">
+                <a
+                  style={{ marginLeft: 0 }}
+                  href={data.link}
+                  class="button is-primary"
+                >
+                  Learn More
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
 )
 
 export default Primary
--- src/components/Primary.js
+++ src/components/Primary.js
@@ -2,30 +2,26 @@ import React from 'react'
 import Details from '../components/Details'
 
 const Primary = ({ data, details }) => (
-  <section className="section section--gradient">
-    <div className="container">
-      <div className="section">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="content">
-              <div className="content">
-                <div className="tile">
-                  <h1 className="title">{data.heading}</h1>
-                </div>
-                <div className="tile">
-                  <h3 className="subtitle">{data.subheading}</h3>
-                </div>
-                <div className="tile">
-                  <p className="subtitle">{data.description}</p>
-                </div>
-              </div>
-              <Details data={details.list} />
+  <div className="container">
+    <div className="section">
+      <div className="columns">
+        <div className="column is-10 is-offset-1">
+          <div className="content">
+            <div className="tile">
+              <h1 className="title">{data.heading}</h1>
             </div>
+            <div className="tile">
+              <h3 className="subtitle">{data.subheading}</h3>
+            </div>
+            <div className="tile">
+              <p className="subtitle">{data.description}</p>
+            </div>
+            <Details data={details.list} />
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
 )
 
 export default Primary
